adeb52cf8654bd61901169508efda977a1440f39
HalidÜnal <halidunal@gmail.com>
2023-12-10 10:54:34 +0300

    mail sender bug fixed

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -2,14 +2,17 @@ const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
 
 const sendEmail = async (mailOptions) => {
-    const transporter = nodemailer.createTransport(smtpTransport({
-        service: "gmail",
-        host: "smtp.gmail.com",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    }))
+    const transporter = nodemailer.createTransport(
+        smtpTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            secure: true,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+    )
 
     transporter.sendMail(mailOptions, (error, info) => {
         if(error) {
